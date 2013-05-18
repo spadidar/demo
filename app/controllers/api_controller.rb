1ef0af595a384a0377a64c05282b2a976a0e38b8
Sasan <siesonz@gmail.com>
2013-05-18 13:25:29 -0700

    models etc

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,11 +2,12 @@ class ApiController < ApplicationController
   
   def store
     data = params["data"]
-    
+    Officer.store data
     render :nothing => true, :status => 200, :content_type => 'text/html'
   end
 
   def show
+    @officers = Officer.all
   end
 
 end
---
+++ app/models/officer.rb
@@ -0,0 +1,22 @@
+class Officer
+
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name,         :type => String,:default => ""
+  field :location,  :type => String,:default => ""
+  field :orientation,        :type => String,:default => ""
+  field :running,         :type => String,:default => ""
+  field :shots,         :type => String, :default => ""
+
+  def self.store json
+    o = Officer.find_or_create_by(name: json["name"])
+    o.location = json["location"]
+    o.orientation = json["orientation"]
+    o.running = json["running"]
+    o.shots = json["shots"]
+    o.save
+    return o
+  end
+
+end
